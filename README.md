456ac2820bb29be5bdc93b8c030a2ffcd9d4a33e
sagie gur ari <sagiegurari@gmail.com>
2018-02-23 18:27:47 +0200

    update lint rules

--- README.md
+++ README.md
@@ -58,7 +58,7 @@ See [contributing guide](.github/CONTRIBUTING.md)
 
 | Date        | Version | Description |
 | ----------- | ------- | ----------- |
-| 2017-12-17  | v0.1.10 | Maintenance |
+| 2018-02-23  | v0.1.11 | Maintenance |
 | 2017-10-10  | v0.1.1  | Update exposed types. |
 | 2017-10-10  | v0.1.0  | Initial release. |
 
--- examples/example.rs
+++ examples/example.rs
@@ -5,9 +5,26 @@ fn main() {
 
     println!("Version: {}", rust_info.version.unwrap());
     println!("Channel: {:#?}", rust_info.channel.unwrap());
-    println!("Target Arch: {}", rust_info.target_arch.unwrap_or("unknown".to_string()));
-    println!("Target Env: {}", rust_info.target_env.unwrap_or("unknown".to_string()));
-    println!("Target OS: {}", rust_info.target_os.unwrap_or("unknown".to_string()));
-    println!("Target Pointer Width: {}", rust_info.target_pointer_width.unwrap_or("unknown".to_string()));
-    println!("Target Vendor: {}", rust_info.target_vendor.unwrap_or("unknown".to_string()));
+    println!(
+        "Target Arch: {}",
+        rust_info.target_arch.unwrap_or("unknown".to_string())
+    );
+    println!(
+        "Target Env: {}",
+        rust_info.target_env.unwrap_or("unknown".to_string())
+    );
+    println!(
+        "Target OS: {}",
+        rust_info.target_os.unwrap_or("unknown".to_string())
+    );
+    println!(
+        "Target Pointer Width: {}",
+        rust_info
+            .target_pointer_width
+            .unwrap_or("unknown".to_string())
+    );
+    println!(
+        "Target Vendor: {}",
+        rust_info.target_vendor.unwrap_or("unknown".to_string())
+    );
 }
